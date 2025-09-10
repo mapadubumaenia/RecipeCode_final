02fea265c2c90265652c7285778c3ead18a338df
MAFA <1000seop@gmail.com>
2025-09-10 11:55:32 +0900

    파일 sample 추가

--- src/test/java/com/RecipeCode/teamproject/TeamprojectApplicationTests.java
+++ src/test/java/com/RecipeCode/teamproject/TeamprojectApplicationTests.java
@@ -2,7 +2,9 @@ package com.RecipeCode.teamproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootTest
 class TeamprojectApplicationTests {
 
